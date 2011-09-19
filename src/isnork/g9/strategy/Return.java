bfb3ca9083f86025b5c274d583163e3ba15c86c3
Yufei Liu <liuyufei@gmail.com>
2011-09-19 09:51:19 -0400

    added return strategy

--- src/isnork/g9/strategy/Return.java
+++ src/isnork/g9/strategy/Return.java
@@ -1,5 +1,35 @@
 package isnork.g9.strategy;
 
-public class Return {
+import java.awt.geom.Point2D;
+import java.util.List;
 
+import isnork.g9.PlayerPrototype;
+import isnork.sim.GameObject.Direction;
+
+public class Return {
+	private PlayerPrototype player;
+	
+	public void setPlayer(PlayerPrototype p) {
+		player = p;
+	}
+	
+	public Direction getDirection() {
+		List<Direction> dirs = Direction.allBut(null);
+		
+		Point2D home = new Point2D.Double(0,0);
+		Point2D cur = player.getLocation();
+		
+		if (cur.getX()==0 && cur.getY()==0) {
+			return Direction.STAYPUT;
+		}
+		
+		for (Direction d : dirs) {
+			Point2D newloc = new Point2D.Double(cur.getX() + d.dx, cur.getY() + d.dy);
+			
+			if (home.distance(newloc) < home.distance(cur)) 
+				return d;
+		}
+		
+		return Direction.STAYPUT;
+	}
 }
